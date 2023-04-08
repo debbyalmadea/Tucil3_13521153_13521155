53d3e9c6853363af021ec706e969df432cded88f
debbyalmadea <almadeaputri@gmail.com>
2023-04-08 20:54:44 +0700

    add: show graph and map

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://cdn.anychart.com/releases/v8/js/anychart-base.min.js"
+        type="text/typescript"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
