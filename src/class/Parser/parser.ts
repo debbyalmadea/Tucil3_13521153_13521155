429e123185e556c66f9cfcd70adeb88179bd2f45
debbyalmadea <almadeaputri@gmail.com>
2023-04-09 19:44:37 +0700

    add: file validation

--- src/class/Parser/parser.ts
+++ src/class/Parser/parser.ts
@@ -13,31 +13,35 @@ export class Parser {
 
     const splitLine = filecontent.split("\n");
 
-    const vertexCount = parseInt(splitLine[0]);
-    const vertexes: Vertex[] = [];
-    for (let i = 1; i < vertexCount + 1; i++) {
-      const splitWord = splitLine[i].split(" ");
-      const vertex = new Vertex(
-        splitWord[0],
-        parseFloat(splitWord[1]),
-        parseFloat(splitWord[2])
-      );
-      vertexes.push(vertex);
-      graph.addVertex(vertex);
-    }
-
-    // read adjacency matrix
-    for (let r = 0; r < vertexCount; r++) {
-      const vertex1 = vertexes[r];
-      const row = splitLine[r + vertexCount + 1].split(" ");
-      for (let c = 0; c < vertexCount; c++) {
-        if (parseInt(row[c]) === 1) {
-          const vertex2 = vertexes[c];
-          graph.addEdge(vertex1, vertex2);
+    try {
+      const vertexCount = parseInt(splitLine[0]);
+      const vertexes: Vertex[] = [];
+      for (let i = 1; i < vertexCount + 1; i++) {
+        const splitWord = splitLine[i].split(" ");
+        const vertex = new Vertex(
+          splitWord[0],
+          parseFloat(splitWord[1]),
+          parseFloat(splitWord[2])
+        );
+        vertexes.push(vertex);
+        graph.addVertex(vertex);
+      }
+  
+      // read adjacency matrix
+      for (let r = 0; r < vertexCount; r++) {
+        const vertex1 = vertexes[r];
+        const row = splitLine[r + vertexCount + 1].split(" ");
+        for (let c = 0; c < vertexCount; c++) {
+          if (parseInt(row[c]) === 1) {
+            const vertex2 = vertexes[c];
+            graph.addEdge(vertex1, vertex2);
+          }
         }
       }
+  
+      return graph;
+    } catch {
+      throw "Invalid file input"
     }
-
-    return graph;
   }
 }
