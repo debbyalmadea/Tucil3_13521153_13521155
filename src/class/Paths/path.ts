285001e34890fa77cb5c3476c41e867639d67fad
Kandida Gunawangit config --global user.name Kandida <kandidagunawan@gmail.com>
2023-04-09 13:32:26 +0700

    add ucs & a* algorithm

--- src/class/Paths/path.ts
+++ src/class/Paths/path.ts
@@ -1,21 +1,21 @@
 import { Graph } from "../Graphs/graph";
 import { Vertex } from "../Graphs/vertex";
 
-interface CostFunc {
-  (vertex: Vertex, lastVertex: Vertex | null): number;
-}
 
 class Path {
   private _path: string[] = [];
   private _cost: number = 0;
   private _distance: number= 0;
 
-  constructor(private graph: Graph, private costFunc: CostFunc) {}
+  constructor(private graph: Graph, private _goalName : string) {}
 
   public get path(): string[] {
     return this._path;
   }
 
+  public get graph_map() : Graph{
+    return this.graph;
+  }
   public get cost(): number {
     return this._cost;
   }
@@ -27,22 +27,27 @@ class Path {
   public get lastVertex(): string {
     return this._path[this._path.length - 1];
   }
-
-  public add(vertexName: string): void {
+  
+  public add(vertexName: string, isUCS : boolean): void {
     const vertexObj = this.graph.getVertexObj(vertexName);
     if (vertexObj != null) {
         let lastVertexObj: Vertex | null = null;
         if (this._path.length > 0) {
             lastVertexObj = this.graph.getVertexObj(this.lastVertex);
-            this._distance += vertexObj.distanceWith(lastVertexObj!);
+            this._cost += vertexObj.distanceWith(lastVertexObj!);
         }
-        this._cost += this.costFunc(vertexObj, lastVertexObj);
+        if(isUCS){
+            this._distance = 0;
+        }
+        else{
+            this._distance = vertexObj.distanceWith(this.graph.getVertexObj(this._goalName)!);
+        }
         this._path.push(vertexName);
     }
   }
 
   public copy(): Path {
-    const copyPath = new Path(this.graph, this.costFunc);
+    const copyPath = new Path(this.graph, this._goalName);
     copyPath._cost = this.cost;
     copyPath._distance = this.distance;
     copyPath._path = [...this._path];
